f2a125557e2345e63b08cbc0c21419f4f9f048ce
DusanGligorijevic <dukacode@gmail.com>
2021-11-18 20:10:01 +0100

    ss

--- google-api/src/main/java/googledriveapi/GoogleDriveImpl.java
+++ google-api/src/main/java/googledriveapi/GoogleDriveImpl.java
@@ -105,62 +105,131 @@ public class GoogleDriveImpl extends Storage{
 
 	@Override
 	public void preview() {
-		try {
-		Drive service = getDriveService();
-
-		FileList result = service.files().list()
-			.setPageSize(10)
-			.setFields("nextPageToken, files(id, name)")
-			.execute();
-		List<File> files = result.getFiles();
-		if (files == null || files.isEmpty()) {
-			System.out.println("No files found.");
-		} else {
-			System.out.println("Files:");
-			for (File file : files) {
-				System.out.printf("%s (%s)\n", file.getName(), file.getId());
-			}
-		}
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
+		
 	
 	}
 
-
-
 	@Override
-	public void preview(java.io.File arg0) {
-		// TODO Auto-generated method stub
+	public void previewAll(String path) {
+		try {
+			Drive service = getDriveService();
+
+			FileList result = service.files().list()
+				.setPageSize(10)
+				.setFields("nextPageToken, files(id, name)")
+				.execute();
+			List<File> files = result.getFiles();
+			if (files == null || files.isEmpty()) {
+				System.out.println("No files found.");
+			} else {
+				System.out.println("Files:");
+				for (File file : files) {
+					System.out.printf("%s (%s)\n", file.getName(), file.getId());
+				}
+			}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
 		
 	}
 
 	@Override
-	public void create(String path, int maxFolders) {
-		// TODO Auto-generated method stub
+	public void createFiles(String path, String name, int maxFolders) {
+		File fileMetadata = new File();
+		fileMetadata.setName(name);
+		fileMetadata.setMimeType("application/vnd.google-apps.folder");
+		try {
+			File file = getDriveService().files().create(fileMetadata)
+				    .setFields("id")
+				    .execute();
+				System.out.println("Folder ID: " + file.getId());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void delete(String path) {
+		try {
+			Drive service = getDriveService();
+			 service.files().delete(path).execute();   
+		} catch (IOException e) {
+			System.out.println("An error occurred: " + e);
+		} 
+		
+	}
+
+	@Override
+	public void transfer(String location, String destination, String DriveFile) {
+		//String fileId = "1sTWaJ_j7PkjzaBWtNc3IzovK5hQf21FbOw9yLeeLPNQ";
+		//String folderId = "0BwwA4oUTeiV1TGRPeTVjaWRDY1E";
+		// Retrieve the existing parents to remove
+		try {
+			File file = getDriveService().files().get(DriveFile)
+				    .setFields("parents")
+				    .execute();
+				StringBuilder previousParents = new StringBuilder();
+				for (String parent : file.getParents()) {
+				  previousParents.append(parent);
+				  previousParents.append(',');
+				}
+				// Move the file to the new folder
+				file = getDriveService().files().update(DriveFile, null)
+				    .setAddParents(destination)
+				    .setRemoveParents(previousParents.toString())
+				    .setFields("id, parents")
+				    .execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+
+	@Override
+	public void previewExt(String f, String s) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void transfer(String location, String destination, String file) {
+	public void previewDir(String path) {
+		try {
+			Drive service = getDriveService();
+			FileList result = service.files().list()
+				.setPageSize(10)
+				.setFields("nextPageToken, files(id, name)")
+				.execute();
+			List<File> files = result.getFiles();
+			if (files == null || files.isEmpty()) {
+				System.out.println("No files found.");
+			} else {
+					System.out.println("Folders: ");
+					for (File folder : files) {
+						if(folder.getMimeType().contentEquals("application/vnd.google-apps.folder"))
+							System.out.printf("%s (%s)\n", folder.getName(), folder.getId());	
+				}
+			
+				System.out.println("Files:");
+				for (File file : files) {
+					System.out.printf("%s (%s)\n", file.getName(), file.getId());
+				}
+			}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		
+	}
+
+	@Override
+	public void download(String path) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void preview(java.io.File f, String s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void preview(java.io.File f, boolean directoriesOnly) {
+	public void createFolders(String path,String name, String number) {
 		// TODO Auto-generated method stub
 		
 	}
